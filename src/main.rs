cf5455ad501b8173e0bb4e87800638f17c70f1cf
edzchen <edzchen747@gmail.com>
2026-05-05 22:26:57 +0100

    speed up startup

--- src/main.rs
+++ src/main.rs
@@ -18,15 +18,14 @@ fn main() {
 fn start_razer_service() {
     let device_pid = razer::device_handle::device().get_pid();
     win::key_hook::init_keyboard_hooks(device_pid);
-    win::power::PowerMonitor::new(); // start power monitor before intialize so we know which power state config to intialise
-    razer::device_handle::device().initialize();
+    win::power::PowerMonitor::new();
     win::standby::spawn_listener_thread();
     win::external_events::spawn_detect_external_updates_thread();
 }
 
 fn close_running_instances() {
     let mut sys = System::new_all();
-    sys.refresh_all();
+    sys.refresh_processes();
 
     let current_pid = sysinfo::get_current_pid().expect("Failed to get app PID");
     let my_name = "hidapi.exe";
